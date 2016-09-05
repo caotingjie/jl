dfb2c77fbc1c72e9b43be9ee1f45ceec89a3c0bf
ajie <ctjprogrammer@163.com>
2016-09-05 22:30:31 +0800

    contextConfigLocation包含Dao和Service层 DispatcherServlet包含的是web应用层（controller）这样我们使用其他web框架的时候也方便，比如不用spring了。解决maven不打jar到web-inf下的lib中 ssm的pom中少了packing标签

--- javaSE/src/main/java/com/ctj/proxyTest/ProxyTest.java
+++ javaSE/src/main/java/com/ctj/proxyTest/ProxyTest.java
@@ -1,13 +1,11 @@
 package com.ctj.proxyTest;
 
-import org.junit.Test;
 
 /**
  * Created by AJie on 2016/6/11.
  */
 public class ProxyTest {
 
-    @Test
     public void testProxy(){
         ProxyFactory proxyFactory = new ProxyFactory();
         UserService userService = (UserService)proxyFactory.createProxyBean(new UserServiceBean("xiaoming"));
