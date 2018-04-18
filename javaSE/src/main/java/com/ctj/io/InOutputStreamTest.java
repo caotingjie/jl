839b313e8c441d1da9df984617c2ab167fcee094
macycao <macycao@blackfish.cn>
2018-04-18 14:13:48 +0800

    JL poi

---
+++ javaSE/src/main/java/com/ctj/io/InOutputStreamTest.java
@@ -0,0 +1,20 @@
+package com.ctj.io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class InOutputStreamTest {
+    public static void main(String[] args) {
+        String s = "20180408";
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            System.out.println(df.format(df.parse(s)));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
